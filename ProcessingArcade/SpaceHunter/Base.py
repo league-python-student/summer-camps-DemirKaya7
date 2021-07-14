1ea75c357aa11eed28f92e9224795ea768652a6e
DemirKaya7 <74899222+DemirKaya7@users.noreply.github.com>
2021-07-13 18:27:34 -0700

    July 13 2021
    July 13 2021

---
+++ ProcessingArcade/SpaceHunter/Base.py
@@ -0,0 +1,9 @@
+class Base:
+    hp = 5
+    img = loadImage("base.jpg")
+    
+    def __init__(self):
+        self.x = width/2
+        self.y = height/2
+        self.hp = 5
+        self.img = loadImage("base.jpg")
